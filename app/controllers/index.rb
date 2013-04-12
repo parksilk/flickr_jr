0f9e754e2b6a1d52a0f84e21c30aa53e5778750d
Apprentice <apprentice@dbc06.local>
2013-04-11 21:41:38 -0700

    thumbnails working (thanks kerrie)

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 get '/' do
-  # Look in app/views/index.erb
+  @photos = Photo.all
   erb :index
 end
 
@@ -13,6 +13,13 @@ post '/upload' do
   photo = Photo.new
   photo.image = params[:photofile]
   photo.save
-  puts photo.inspect
-  "Success! <img src='#{photo.image}'>"
+
+  image = MiniMagick::Image.open(photo.image.current_path)
+  image.resize "100x100"
+  thumb_url = photo.image.current_path.split(".").first + "_thumb." + photo.image.current_path.split(".").last
+  image.write thumb_url
+
+  Photo.create(:image => image)
+
+  redirect '/'
 end
